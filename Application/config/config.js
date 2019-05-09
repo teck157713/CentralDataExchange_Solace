5888d1a4dc0c2bafe94a069a1ef384c499852217
teck157713 <teck.157713@hotmail.com>
2019-05-09 09:28:26 +0800

    Create config.js

---
+++ Application/config/config.js
@@ -0,0 +1,6 @@
+var account = {
+    HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
+    VPN:        'msgvpn-jfgwkefwydv',
+    USERNAME:   'solace-cloud-client',
+    PASS:       '9bsjj5sg7al2rj1ne5tkack131'
+}
