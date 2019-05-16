dedd28cc0a09e5ac18fc6029b24b690aac9eef48
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-05-16 16:47:50 +0800

    UI

--- Application/ui.js
+++ Application/ui.js
@@ -10,4 +10,12 @@ function openStuff(evt, name) {
     }
     document.getElementById(name).style.display = "block";
     evt.currentTarget.className += " w3-red";
-  }
+  };
+
+  function connect() {
+    var hosturl = account.HOSTURL;
+    var username = account.USERNAME;
+    var vpn = account.VPN;
+    var connect = document.getElementById('connect');
+    connect.innerHTML = 'Username: '+username+'<br>'+'Message URL: '+hosturl+'<br>'+'VPN Name: '+vpn;
+  };
