2048bc9c8c86c837aa6e1e673ebc964069dd4074
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-05-30 13:10:53 +0800

    add sdkperf

--- Application/pub.bat
+++ Application/pub.bat
@@ -3,6 +3,5 @@ ECHO Starting Publishing.
 
 for /f "usebackq tokens=1-2 delims=," %%a in ("hello.csv") do (
       echo %%a> txt.txt 
-      sdkperf_c -cip=tcp://mr8ksiwsp23vv.messaging.solace.cloud:20448 -cu=solace-cloud-client@msgvpn-jfgwkeg1ahl -cp=croo89sd2pkbmk49a325m21q1v -stl=a/topic -ptl=a/topic -mn=1 -md -pal=txt.txt )
-
+      sdkperf_c -cip=tcp://mr8ksiwsp23vv.messaging.solace.cloud:20448 -cu=solace-cloud-client@msgvpn-jfgwkeg1ahl -cp=croo89sd2pkbmk49a325m21q1v -stl=a/topic -ptl=a/topic -mn=1 -md -pal=txt.txt)
 PAUSE
