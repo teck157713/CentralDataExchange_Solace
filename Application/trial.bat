4441b1bd8bccec7468069024c7a31b4703253174
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-06-06 10:26:22 +0800

    sdkperf

--- Application/trial.bat
+++ Application/trial.bat
@@ -2,10 +2,9 @@
 setlocal enabledelayedexpansion
 ECHO Starting Publishing.
 
-for /f "usebackq tokens=1-4 delims=," %%1 in ("TempData.csv") do (
-      for /f "usebackq tokens=1-2 delims=," %%a in ("latlon.csv") do (
-      set "message="id":"%%1","lat":"%%2","long":"%%3","value":"%%4""
+for /f "usebackq tokens=1-5 delims=," %%a in ("TempData.csv") do (
+      set "message="id":"%%a","lat":"%%b","long":"%%c","value":"%%d","timestamp":"%%e""
       echo !message!>txt.txt
-      sdkperf_c -cip=tcp://mr8ksiwsp23vv.messaging.solace.cloud:20448 -cu=solace-cloud-client@msgvpn-jfgwkeg1ahl -cp=croo89sd2pkbmk49a325m21q1v -stl=NEA/1/temp_data/raw/%%a/%%b -ptl=NEA/1/temp_data/raw/%%a/%%b -mn=1 -md -pal=txt.txt ))
+      sdkperf_c -cip=tcp://mr8ksiwsp23vv.messaging.solace.cloud:20448 -cu=solace-cloud-client@msgvpn-jfgwkeg1ahl -cp=croo89sd2pkbmk49a325m21q1v -stl=NEA/1/temp_data/raw/%%b/%%c -ptl=NEA/1/temp_data/raw/%%b/%%c -mn=1 -md -pal=txt.txt )
     REM sdkperf_c -cip=ws://mr8ksiwsp23vv.messaging.solace.cloud:20483 -cu=solace-cloud-client@msgvpn-jfgwkefwydv -cp=9bsjj5sg7al2rj1ne5tkack131 -stl=NEA/1/temp_data/raw -ptl=NEA/1/temp_data/raw -mn=10 -md -pal=txt.txt )
 PAUSE
