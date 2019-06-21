0a962197628bb380bb6258bc6786a9cdb356e6b3
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-06-21 16:43:41 +0800

    Update microsoftanalyse.js

--- Application/microsoftanalyse.js
+++ Application/microsoftanalyse.js
@@ -8,7 +8,7 @@ function processImage(sourceImageUrl, location, callback) {
 
   // You must use the same Azure region in your REST API method as you used to
   // get your subscription keys. For example, if you got your subscription keys
-  // from the West US region, replace "westcentralus" in the URL
+  // from the West US region, replace "southeastasia" in the URL
   // below with "westus".
   //
   // Free trial subscription keys are generated in the "westus" region.
