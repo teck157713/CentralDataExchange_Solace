7025fbaef0222c87454590c2135d40abeafc107f
rajiv <rajivxavier17@hotmail.com>
2019-06-21 15:29:54 +0800

    local vmr setup

--- Application/config/config.js
+++ Application/config/config.js
@@ -1,6 +1,10 @@
 var account = {
-    HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
-    VPN:        'msgvpn-jfgwkefwydv',
-    USERNAME:   'solace-cloud-client',
-    PASS:       '9bsjj5sg7al2rj1ne5tkack131'
+    HOSTURL:    'ws://localhost',
+    // ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
+    VPN:     'default'  ,
+    // 'msgvpn-jfgwkefwydv',
+    USERNAME:   'default',
+    // 'solace-cloud-client',
+    PASS:       'secret'
+    // '9bsjj5sg7al2rj1ne5tkack131'
 }
