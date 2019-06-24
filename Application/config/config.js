69d91573a4820370f533957ec4e7e22d447385c8
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-06-24 13:27:56 +0800

    Update config.js

--- Application/config/config.js
+++ Application/config/config.js
@@ -1,10 +1,14 @@
 var account = {
-    HOSTURL:    'ws://localhost',
-    // ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
-    VPN:     'default'  ,
-    // 'msgvpn-jfgwkefwydv',
-    USERNAME:   'default',
-    // 'solace-cloud-client',
-    PASS:       'secret'
-    // '9bsjj5sg7al2rj1ne5tkack131'
+    HOSTURL:    
+    // 'ws://localhost',
+    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
+    VPN:     
+    // 'default'  ,
+    'msgvpn-jfgwkefwydv',
+    USERNAME:  
+    //  'default',
+    'solace-cloud-client',
+    PASS:       
+    // 'secret'
+    '9bsjj5sg7al2rj1ne5tkack131'
 }
