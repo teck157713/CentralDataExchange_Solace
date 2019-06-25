22d33616523ae1edb3451668cf37ee480b43bc88
Khu Giem Teck <khu.teck@solace.com>
2019-06-25 14:26:58 +0800

    Update config.js

--- Application/config/config.js
+++ Application/config/config.js
@@ -1,6 +1,6 @@
 var account = {
-    HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
-    VPN:        'msgvpn-jfgwkefwydv',
-    USERNAME:   'solace-cloud-client',
-    PASS:       '9bsjj5sg7al2rj1ne5tkack131'
+    HOSTURL:    'ws://localhost',
+    VPN:        'default',
+    USERNAME:   'admin',
+    PASS:       'admin'
 }
