517539f603eee02fde2d6239bad5b26652b7bb55
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-06-26 10:35:33 +0800

    Update config.js

--- Application/config/config.js
+++ Application/config/config.js
@@ -3,4 +3,9 @@ var account = {
     VPN:        'default',
     USERNAME:   'admin',
     PASS:       'admin'
+
+    // HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
+    // VPN:        'msgvpn-jfgwkefwydv',
+    // USERNAME:   'solace-cloud-client',
+    // PASS:       '9bsjj5sg7al2rj1ne5tkack131'
 }
