65024042145beabece4f85d8ed82da527636557d
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-06-28 09:56:13 +0800

    Update ui.js

--- Application/ui.js
+++ Application/ui.js
@@ -47,18 +47,11 @@ function mySearch(inputs,tables) {
   }
 }
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+function getCookie(name) {
+  var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+  return v ? v[2] : null;
+}
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + value;
 }
