9f9cbad1d8cbc407a368429662ae56fea8f4c297
rax1795 <31851604+rax1795@users.noreply.github.com>
2019-07-16 11:21:19 +0800

    Update log.js

--- Application/js/log.js
+++ Application/js/log.js
@@ -51,9 +51,11 @@ var QueueConsumer = function (queueName, table, logs) {
                 var cell1 = row.insertCell(0);
                 var cell2 = row.insertCell(1);
                 var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
                 cell1.innerHTML = arr[0];
                 cell2.innerHTML = arr[1];
                 cell3.innerHTML = arr[2];
+                cell4.innerHTML = <input type="button" name="del" value="NO" id=arr />;
             } catch (error) {
                 consumer.log(error.toString());
             }
