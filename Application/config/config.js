09202b4f150197d178e469a9a0e7ced0f0a1a5fa
Khu Giem Teck <khu.teck@solace.com>
2019-08-15 10:06:49 +0800

    Update config.js

--- Application/config/config.js
+++ Application/config/config.js
@@ -1,48 +1,38 @@
+// Central Broker (localhost): required
 var account = {
-    HOSTURL:    'ws://localhost',
-    VPN:        'default',
-    USERNAME:   'admin',
-    PASS:       'admin',
-    PUBLICURL:  'v:4b427c610209'
+    HOSTURL:    '',
+    VPN:        '',
+    USERNAME:   '',
+    PASS:       '',
+    PUBLICURL:  ''
 }
-
+// Represents one agency: optional
 var LTA = {
-    HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20483',
-    VPN:        'msgvpn-jfgwkefwydv',
-    remoteMsgVpnLocation:     'mr8ksiwsp23vv.messaging.solace.cloud:20480',
-    USERNAME:   'solace-cloud-client',
-    PASS:       '9bsjj5sg7al2rj1ne5tkack131',
-    SEMPURL:    'mr8ksiwsp23vv.messaging.solace.cloud:20494',
-    SEMPNAME:   'msgvpn-jfgwkefwydv-admin',
-    SEMPPASS:   '5ugnmo24ab72hd2fu5a84r1nfc'
+    HOSTURL:    '',
+    VPN:        '',
+    remoteMsgVpnLocation:     '',
+    USERNAME:   '',
+    PASS:       '',
+    SEMPURL:    '',
+    SEMPNAME:   '',
+    SEMPPASS:   ''
 }
-
+// Represents another agency: optional
 var NEA = {
-    NAME:       'NEA',
-    HOSTURL:    'ws://mr8ksiwsp23vv.messaging.solace.cloud:20451',
-    VPN:        'msgvpn-jfgwkeg1ahl',
-    USERNAME:   'solace-cloud-client',
-    PASS:       'croo89sd2pkbmk49a325m21q1v',
-    SEMPURL:    'mr8ksiwsp23vv.messaging.solace.cloud:20462',
-    SEMPNAME:   'msgvpn-jfgwkeg1ahl-admin',
-    SEMPPASS:   'j2h5k373182c86h1nsa1quismk'
-}
-var newReg = {
-    NAME:       'new',
-    HOSTURL:    'ws://mrzpfs1b9gh81.messaging.solace.cloud:20291',
-    remoteMsgVpnLocation:   'mrzpfs1b9gh81.messaging.solace.cloud:20288',
-    VPN:        'msgvpn-zpfs1b9w8wz',
-    USERNAME:   'solace-cloud-client',
-    PASS:       '4j4tmhhldn6kklhlvnq4ldsvmm',
-    SEMPURL:    'mrzpfs1b9gh81.messaging.solace.cloud:20302',
-    SEMPNAME:   'msgvpn-zpfs1b9w8wz-admin',
-    SEMPPASS:   'jd1hu487v6he5hotnr1hanl2ab'
+    NAME:       '',
+    HOSTURL:    '',
+    VPN:        '',
+    USERNAME:   '',
+    PASS:       '',
+    SEMPURL:    '',
+    SEMPNAME:   '',
+    SEMPPASS:   ''
 }
 // Microsoft Computer Vision API, Change if necessary
 var AzureAPI = {
-    subscriptionKey:    '3ba325cc4e724a70b6a73b0395f262bb',
+    subscriptionKey:    '',
 }
 
 var GoogleMapAPI = {
-    subscriptionKey:    'AIzaSyDzHU1Oi7smE91XPeiY43wsRXdPG9RqzV8',
+    subscriptionKey:    '',
 }
